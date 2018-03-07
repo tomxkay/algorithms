58762f06e3beb9be2890cf1388436bfcd1e3f533
Thomas Kay <t2uan.k@gmail.com>
2018-03-06 19:20:09 -0500

    implement insertion/selection sorting algorithm

---
+++ sorting/insertion/insertionSort.js
@@ -0,0 +1,46 @@
+function ArrayList() {
+  
+  const array = [];
+
+  this.insert = function(item) {
+    array.push(item);
+  };
+
+  this.toString = function() {
+    return array.join();
+  };
+
+  this.insertionSort = function() {
+    const length = array.length;
+    let j, temp;
+
+    for (let i=1; i<length; i++) {
+      j = i;
+      temp = array[i];
+
+      while (j>0 && array[j-1] > temp) {
+        array[j] = array[j-1];
+        j--;
+      }
+
+      array[j] = temp;
+    }
+  };
+
+}
+
+function createNonSortedArray(size) {
+  const array = new ArrayList();
+
+  for (let i=size; i>0; i--) {
+    array.insert(i);
+  }
+
+  return array;
+}
+
+const array = createNonSortedArray(7);
+console.log(array.toString());
+
+array.insertionSort();
+console.log(array.toString());
---
+++ sorting/selection/selectionSort.js
@@ -0,0 +1,58 @@
+
+const swap = function(array, index1, index2) {
+
+  const aux = array[index1];
+  array[index1] = array[index2];
+  array[index2] = aux;
+}
+
+
+
+
+function ArrayList() {
+  
+  const array = [];
+
+  this.insert = function(item) {
+    array.push(item);
+  };
+
+  this.toString = function() {
+    return array.join();
+  };
+
+  this.selectionSort = function() {
+    const length = array.length;
+    let indexMin;
+
+    for (let i=0; i<length; i++) {
+      indexMin = i;
+      for (let j=i; j<length; j++) {
+        if (array[indexMin] > array[j]) {
+          indexMin = j;
+        }
+      }
+      if (i !== indexMin) {
+        swap(array, i, indexMin);
+      }
+    }
+  };
+  
+}
+
+
+function createNonSortedArray(size) {
+  const array = new ArrayList();
+
+  for (let i=size; i>0; i--) {
+    array.insert(i);
+  }
+
+  return array;
+}
+
+const array = createNonSortedArray(5);
+console.log(array.toString());
+
+array.selectionSort();
+console.log(array.toString());
