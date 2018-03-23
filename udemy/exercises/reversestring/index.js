4b31d04e6a0fa74131359b8c157938c2947950cc
Thomas Kay <t2uan.k@gmail.com>
2018-03-23 13:19:22 -0400

    add more common algorithms and implemented reversestring

---
+++ udemy/exercises/reversestring/index.js
@@ -0,0 +1,36 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '');
+}
+
+
+
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
+
+
+
+
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+module.exports = reverse;
+
+
