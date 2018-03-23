5fb2ba5a0e603127e2f0b462c7ea44610a7186be
Thomas Kay <t2uan.k@gmail.com>
2018-03-23 15:11:03 -0400

    add reverseint algorithm

--- udemy/exercises/reverseint/index.js
+++ udemy/exercises/reverseint/index.js
@@ -8,6 +8,21 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversedNum = Math.abs(n).toString().split('').reverse().join('');
+
+  return parseInt(reversedNum) * Math.sign(n); // Math.sign() takes a number and return 1 if that number is positive, otherwise -1.
+}
+
+
+// function reverseInt(n) {
+//   const reversedNum = Math.abs(n).toString().split('').reverse().join('');
+
+//   if (n < 0) {
+//     return parseInt(reversedNum) * -1;
+//   } else {
+//     return parseInt(reversedNum);
+//   }
+// }
 
 module.exports = reverseInt;
