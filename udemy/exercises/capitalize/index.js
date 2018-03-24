434de9edb0eb3fdac0689e1625def6fdf049af15
Thomas Kay <t2uan.k@gmail.com>
2018-03-24 17:09:42 -0400

    add capitalize algorithm

--- udemy/exercises/capitalize/index.js
+++ udemy/exercises/capitalize/index.js
@@ -7,6 +7,45 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  return str
+    .split(" ")
+    .map(word =>
+      word
+        .charAt(0)
+        .toUpperCase()
+        .concat(word.slice(1))
+    )
+    .join(" ");
+}
+
+
+
+// // capitalize the first letter of the sentence and 
+// // then loop through the rest and capitalize every letter that has 
+// // a direct space character behind it
+// function capitalize(str) {
+//   let result = str[0].toUpperCase();
+
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i -1] === ' ') {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//   }
+
+//   return result;
+// }
+
+// function capitalize(str) {
+//   const words = [];
+
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+
+//   return words.join(' ');
+// }
 
 module.exports = capitalize;
