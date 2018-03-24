91f97f09b6dfe1325e1fdc85a41fcfad04450595
Thomas Kay <t2uan.k@gmail.com>
2018-03-23 21:35:21 -0400

    add maxchar algorithm

--- udemy/exercises/maxchar/index.js
+++ udemy/exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) { // for...of is used for strings and arrays
+    charMap[char] ? charMap[char]++ : charMap[char] = 1;
+  }
+
+  for (let char in charMap) {  // for...in is used for objects
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
