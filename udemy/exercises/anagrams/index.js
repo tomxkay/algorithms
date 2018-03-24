aa10cb2e90cefcbd46b98565ec996999718ba61a
Thomas Kay <t2uan.k@gmail.com>
2018-03-24 16:36:55 -0400

    add anagram algorithm

--- udemy/exercises/anagrams/index.js
+++ udemy/exercises/anagrams/index.js
@@ -8,6 +8,52 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+
+
+// helper function to clean string by removing punctuations and sorting it in alphabetical order.
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+  
+
+}
+
+
+
+
+// function buildCharMap(str) {
+//   const re = /[^\w]/g;
+//   const charMap = {};
+
+//   const cleanString = str.replace(re, '').toLowerCase();
+
+//   for (let char of cleanString) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+
+//   return charMap;
+// }
+
+// function anagrams(stringA, stringB) {
+//   const charMapA = buildCharMap(stringA);
+//   const charMapB = buildCharMap(stringB);
+
+
+//   if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//     return false;
+//   }
+
+//   for (prop in charMapA) {
+//     if (charMapA[prop] !== charMapB[prop]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+
+// }
 
 module.exports = anagrams;
