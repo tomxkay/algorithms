fc64b3e91c9db2788dcac66fef4e81ad97c0468f
Thomas Kay <t2uan.k@gmail.com>
2018-03-25 10:34:45 -0400

    add steps algorithm

--- udemy/exercises/steps/index.js
+++ udemy/exercises/steps/index.js
@@ -17,6 +17,58 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+
+//     console.log(stair);
+//   }
+// }
+
+
+
+
+// recursion implementation
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
+}
+
+
+
+
+
+
+
+
+// function steps(n) {
+//   for (let i = 0; i < n; i++) {
+//     const str = "#".repeat(i + 1).concat(" ".repeat(n - (i + 1)));
+
+//     console.log(str);
+//   }
+// }
 
 module.exports = steps;
