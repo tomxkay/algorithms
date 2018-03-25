0166184c2b96c493d31fa545c02c94497ef797e2
Thomas Kay <t2uan.k@gmail.com>
2018-03-25 12:36:50 -0400

    add pyramid algorithm

--- udemy/exercises/pyramid/index.js
+++ udemy/exercises/pyramid/index.js
@@ -14,6 +14,68 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+
+
+// recursive implementation
+function pyramid(n, row = 0, level = '') {
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2*n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2*n -1) / 2);
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    level += "#"
+  } else {
+    level += ' ';
+  }
+
+  pyramid(n, row, level)
+
+}
+
+// // iteratable implementation
+// function pyramid(n) {
+//   const midpoint = Math.floor((2*n-1) / 2);
+
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+
+//     for (let column = 0; column < 2 * n - 1; column ++) {
+      
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+
+//     }
+
+//     console.log(level);
+
+//   }
+// }
+
+
+
+
+// // my implementation
+// function pyramid(n) {
+
+//   const arr = [];
+//   let rows = n;
+//   let columns = n + (n -1);
+
+//   for (let i = 0; i < n; i++) {
+//       arr.push(' '.repeat(i).concat('#'.repeat(columns - (i*2))).concat(' '.repeat(i)));
+//   }
+
+
+//   arr.reverse().forEach((s) => console.log(s));
+// }
 
 module.exports = pyramid;
