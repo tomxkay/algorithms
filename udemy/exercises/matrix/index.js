f4b8053a867fbd704ff3852117ed6e795be5f6c7
Thomas Kay <t2uan.k@gmail.com>
2018-03-26 13:30:30 -0400

    add spiral matrix algorithm

--- udemy/exercises/matrix/index.js
+++ udemy/exercises/matrix/index.js
@@ -15,6 +15,93 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const results = [];
+  let counter = 1; // start counter at 1;
+  let startRow = 0;
+  let startCol = 0;
+  let endRow = n - 1;
+  let endCol = n - 1;
+
+  // add n number of arrays to the results array
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+
+  // implementation
+  while (startCol <= endCol && startRow <= endRow) {
+    // top row
+    for (let i = startCol; i <= endCol; i++) {
+      results[startRow][i] = counter++;
+    }
+    startRow++;
+
+    // right col
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endCol] = counter++;
+    }
+    endCol--;
+
+    // bottom row
+    for (let i = endCol; i >= startCol; i--) {
+      results[endRow][i] = counter++;
+    }
+    endRow--;
+
+    // left col aka start col
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startCol] = counter++;
+    }
+    startCol++;
+  }
+
+  return results;
+}
+
+// function matrix(n) {
+//   const results = [];
+//   let counter = 1;
+//   let startRow = 0;
+//   let startCol = 0;
+//   let endRow = n - 1;
+//   let endCol = n - 1;
+
+//   for (let i = 0; i < n; i++) {
+//     results.push([]);
+//   }
+
+//   while (startCol <= endCol && startRow <= endRow) {
+//     // top row
+//     for (let i = startCol; i <= endCol; i++) {
+//       results[startRow][i] = counter;
+//       counter++;
+//     }
+//     startRow++;
+
+//     // right col
+//     for (let i = startRow; i <= endRow; i++) {
+//       results[i][endCol] = counter;
+//       counter++;
+//     }
+//     endCol--;
+
+//     // bottom row
+//     for (let i = endCol; i >= startCol; i--) {
+//       results[endRow][i] = counter;
+//       counter++;
+//     }
+//     endRow--;
+
+//     // left col
+//     for (let i = endRow; i >= startRow; i--) {
+//       results[i][startCol] = counter;
+//       counter++;
+//     }
+//     startCol++;
+
+//   }
+
+//   return results;
+// }
 
 module.exports = matrix;
