1e1d863b76e6fbc2aa49ba2b307f4d8334ac99c0
= <t2uan.k@gmail.com>
2018-04-14 00:13:02 -0700

    implemented an algorithm to determine whether a linkedlist is circular

--- udemy/exercises/circular/index.js
+++ udemy/exercises/circular/index.js
@@ -12,6 +12,25 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  // using the same algorithm to find the midpoint,
+  // if slow and fast points to the same node 
+  // after the first iteration, it is a circular linkedlist.
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+        if (slow === fast) {
+          return true;
+        }
+  }
+
+  // if the while loop breaks,
+  // it means that the linkedlist is not cirlular.
+  return false;
+}
 
 module.exports = circular;
