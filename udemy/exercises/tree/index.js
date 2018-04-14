fdd93be5d88b70c794e98958700a6049acfa7753
= <t2uan.k@gmail.com>
2018-04-14 02:38:27 -0700

    implement tree data structure with bredth and depth traversals

--- udemy/exercises/tree/index.js
+++ udemy/exercises/tree/index.js
@@ -10,8 +10,64 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
 
-class Tree {}
+
+
+// breadth first vs. depth first traversal
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter(node => node.data !== data);
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    // add root node to array
+    const arr = [this.root];
+
+    while (arr.length) {
+      // remove the node at index 0
+      const node = arr.shift();
+
+      // add its children nodes to the array
+      // arr.concat(node.children);
+      arr.push(...node.children);
+
+      // call the fn passing in node
+      // which ultimately will traverse through 
+      // all of the nodes
+      fn(node);
+    }
+  }
+
+  traverseDF(fn) {
+    const arr = [this.root];
+
+    // the difference b/t bredth first v. depth first traversal
+    // is bredth first add the node's children node to the end of the array
+    // and depth first add thenode's children node to the start of the array.
+    
+    while (arr.length) {
+      const node = arr.shift();
+      
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }
+}
 
 module.exports = { Tree, Node };
