2a00d062afd0954d9ef6176dd5dfd36b2d0ccb42
= <t2uan.k@gmail.com>
2018-04-14 14:05:51 -0700

    implemented an algorithm to validate a bst data structure

--- udemy/exercises/validate/index.js
+++ udemy/exercises/validate/index.js
@@ -5,6 +5,55 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+
+  // break case: test for invalid bst
+  if (min !== null && node.data < min) {
+    return false;
+  }
+  if (max !== null && node.data > max) {
+    return false;
+  }
+
+  // recursively traverse the bst
+  if (node.left && !validate(node.left, min, node.data)) {
+
+    return false;
+  }
+  if (node.right && !validate(node.right, node.data, max)) {
+
+    return false;
+  }
+
+  // if it hasn't failed by this point, it is a valid bst
+  return true;
+}
+
+
+
+
+// function validate(node, min = null, max = null) {
+
+//   // break case: test for invalid bst
+//   if (min !== null && node.data < min) {
+//     return false;
+//   }
+//   if (max !== null && node.data > max) {
+//     return false;
+//   }
+
+//   // recursively traverse the bst
+//   if (node.left) {
+
+//     return validate(node.left, min, node.data);
+//   }
+//   if (node.right) {
+
+//     return validate(node.right, node.data, max);
+//   }
+
+//   // if it hasn't failed by this point, it is a valid bst
+//   return true;
+// }
 
 module.exports = validate;
