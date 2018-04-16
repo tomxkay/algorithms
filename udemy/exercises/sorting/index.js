d6ddef9a9b7092f81b2d37dbebc322a5ce420aa6
= <t2uan.k@gmail.com>
2018-04-16 11:23:31 -0700

    course complete: all common data structures and algorithms implemented and good for reference

--- udemy/exercises/sorting/index.js
+++ udemy/exercises/sorting/index.js
@@ -2,19 +2,76 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+  const len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < (len - i - 1); j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
 
 }
 
 function selectionSort(arr) {
 
+  const len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    let indexOfMin = i;
+
+    // find the index of the minimum value
+    for (let j = i+1; j < len; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+
+    // if the value at index i is not
+    // equal to the value at indexOfMin, swap them
+    if (indexOfMin !== i) {
+      let temp = arr[i];
+      arr[i] = arr[indexOfMin];
+      arr[indexOfMin] = temp;
+    }
+  }
+
+  return arr;
+
 }
 
 function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  let mid = Math.floor(arr.length / 2);
+
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
 
 }
 
+// function to join together two sorted array in sorted order.
 function merge(left, right) {
+  const results = [];
 
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
 }
 
-module.exports = { bubbleSort, selectionSort, mergeSort };
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
